ba3affff558c7bf90e4c98d0807a454be2b23134
Miguel A. Ledesma <miguelledesma33@gmail.com>
2022-04-14 23:54:09 -0700

    only missing one test for projects

--- api/projects/projects-router.js
+++ api/projects/projects-router.js
@@ -4,6 +4,7 @@ const express = require('express');
 const router = express.Router()
 
 const {validateProjectId, validateProject} = require('./projects-middleware'); 
+const e = require('express');
 
 router.get("/", (req, res) => {
     Projects.get()
@@ -68,7 +69,6 @@ router.put("/:id",validateProjectId ,validateProject,(req, res, next) =>{
 
 
 
-
 router.delete("/:id", validateProjectId, async (req, res, next) => {
     try{
         await Projects.remove(req.params.id)
@@ -76,6 +76,18 @@ router.delete("/:id", validateProjectId, async (req, res, next) => {
     } catch(err){
         next(err)
     }
+})
+
+router.get("/:id/actions", validateProjectId, async (req, res, next) => {
+    Projects.getProjectActions(req.params.id)
+        .then(actions => {
+            if(actions.length > 0){
+                res.status(200).json(actions)
+            } else {
+                res.status(404).json((actions))
+            }
+        })
+        .catch(next)
 })
 
 
