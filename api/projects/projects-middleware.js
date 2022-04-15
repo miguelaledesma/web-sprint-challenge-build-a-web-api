91af4ee7b12bf449eb37d67b1c6a2539b9a89e01
Miguel A. Ledesma <miguelledesma33@gmail.com>
2022-04-14 23:47:58 -0700

    14 tests passing

--- api/projects/projects-middleware.js
+++ api/projects/projects-middleware.js
@@ -16,4 +16,21 @@ async function validateProjectId(req, res, next){
     }
 }
 
-module.exports = {validateProjectId}
+async function validateProject(req, res,next){
+    const {name, description, completed} = req.body
+    if(!name || !name.trim()){
+        res.status(400).json({message: 'Name field required'})
+    } else if(!description || !description.trim()){
+        res.status(400).json({message: 'missing required description field'})
+    } 
+    else{
+        req.name = name.trim()
+        req.description = description.trim()
+        req.completed = completed
+        next()
+    }
+}
+
+
+
+module.exports = {validateProjectId, validateProject}
--- api/projects/projects-router.js
+++ api/projects/projects-router.js
@@ -3,7 +3,7 @@ const Projects = require('./projects-model');
 const express = require('express'); 
 const router = express.Router()
 
-const {validateProjectId} = require('./projects-middleware'); 
+const {validateProjectId, validateProject} = require('./projects-middleware'); 
 
 router.get("/", (req, res) => {
     Projects.get()
@@ -21,6 +21,63 @@ router.get("/:id", validateProjectId, (req,res, next) => {
     } catch(err){
         next(err)
     }
+}); 
+
+router.post("/", (req, res) => {
+    const newProject = req.body; 
+    Projects.insert(newProject)
+    .then(project => {
+        res.status(201).json(newProject)
+    })
+    .catch(err => {
+        res.status(400).json({message: "There was an error adding your project"})
+    })
 })
+
+// router.put("/:id",validateProjectId ,validateProject,(req, res, next) =>{
+//     const {name, description, completed} = req.body; 
+//     if(!name || !description || !completed){
+//         res.status(400).json({message: 'Project ID does not exist'})
+//     } else {
+//         Projects.update(req.params.id ,req.body)
+//         .then(() => {
+//           return Projects.get(req.params.id)
+//         })
+//         .then(project => {
+//             res.json(project)
+//         })
+//         .catch(next)
+//     }
+
+// })
+
+
+router.put("/:id",validateProjectId ,validateProject,(req, res, next) =>{
+
+        Projects.update(req.params.id ,req.body)
+        .then(() => {
+          return Projects.get(req.params.id)
+        })
+        .then(project => {
+            res.json(project)
+        })
+        .catch(next)
+    
+
+})
+
+
+
+
+router.delete("/:id", validateProjectId, async (req, res, next) => {
+    try{
+        await Projects.remove(req.params.id)
+        res.json(res.Projects)
+    } catch(err){
+        next(err)
+    }
+})
+
+
 
 module.exports = router
